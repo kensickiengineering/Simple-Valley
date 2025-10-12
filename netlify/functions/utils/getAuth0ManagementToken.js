426d80855effd61a1f8daa9a8655a343611e6496
kensickiengineering <jaybaseball10@gmail.com>
2025-10-12 16:54:12 -0400

    accounts_v13

--- netlify/functions/utils/getAuth0ManagementToken.js
+++ netlify/functions/utils/getAuth0ManagementToken.js
@@ -7,10 +7,10 @@ module.exports = async () => {
   const clientSecret = process.env.AUTH0_MGT_CLIENT_SECRET;
 
   try {
-    const response = await axios.post(`https://${login.simplevalleybar.com}/oauth/token`, {
+    const response = await axios.post(`https://${domain}/oauth/token`, {
       client_id: clientId,
       client_secret: clientSecret,
-      audience: `https://${login.simplevalleybar.com}/api/v2/`,
+      audience: `https://${dev-y6t0um4ltxspelep.us.auth0.com}/api/v2/`,
       grant_type: 'client_credentials',
     });
     return response.data.access_token;
